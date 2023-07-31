12186887666cc583a3343c7d9298ccb517113a34
BlodgettBenjamin <77547984+BlodgettBenjamin@users.noreply.github.com>
2023-07-31 12:23:29 -0600

    branch explanation

--- src/ben/tree.h
+++ src/ben/tree.h
@@ -6,6 +6,18 @@
 
 namespace btl
 {
+	/*
+	* btl::branch is a node that takes advantage of templates. it points to a parent branch node and non child node
+	* data. it is meant to be used with a data oriented design. it both maintains a pointer to it's specific data
+	* as well as a 64 bit index. this is perhaps redundant and the index might be removed altogether in favor of simply
+	* calculating a difference using the child_data pointer but memory use is not a concern for this class. it is meant
+	* for use in a dynamic tree structure which will lend itself well to serialization and use with data oriented
+	* design. in practice the branches within the same layers will point to entirely different allocations prior to
+	* serialization. during serialization the tree structure can probe the branches to determine a fixed total
+	* allocation and the entire tree with the conceptual branches and layer allocations can be allocated in bulk. no
+	* protocol for a non-linear resizing of memory is used because after serialization the tree is not intended for
+	* applications which require lots of resizing.
+	*/
 	template <typename parent_t, typename child_t>
 	struct branch
 	{
