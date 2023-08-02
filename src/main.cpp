4013616c0d8297fe89ae8f3215ebf18ee5361091
BlodgettBenjamin <77547984+BlodgettBenjamin@users.noreply.github.com>
2023-08-02 12:57:22 -0600

    make a tree but how do I create pointer of runtime specified size??

--- src/main.cpp
+++ src/main.cpp
@@ -26,18 +26,19 @@ struct color
 
 namespace btl
 {
-	template <class sizes_array_t, u64 layer_count_t>
+	template <u64 layer_count_t>
 	struct tree
 	{
 		u64* layer_sizes = nullptr;
 		void** layers = nullptr;
 
 		tree();
-		void add(u8 layer, const void* data_ptr, u64 count = 1);
+		inline u64 layer_count();
+		void add(const u64* const layer_type_sizes, u8 layer_index, const void* data_ptr, u64 count = 1);
 	};
 
-	template <class sizes_array_t, u64 layer_count_t>
-	tree<sizes_array_t, layer_count_t>::tree()
+	template <u64 layer_count_t>
+	tree<layer_count_t>::tree()
 	{
 		layer_sizes = (u64*)calloc(layer_count_t, sizeof(u64));
 		
@@ -50,35 +51,60 @@ namespace btl
 			return;//complain
 	}
 
-	template <class sizes_array_t, u64 layer_count_t>
-	void tree<sizes_array_t, layer_count_t>::add(u8 layer_index, const void* data_ptr, u64 count)
+	template <u64 layer_count_t>
+	inline u64 tree<layer_count_t>::layer_count()
+	{
+		return layer_count_t;
+	}
+
+	template <u64 layer_count_t>
+	void tree<layer_count_t>::add(const u64* const layer_type_sizes, u8 layer_index, const void* data_ptr, u64 count)
 	{
 		layer_sizes[layer_index] += count;
-		layers[layer_index] = realloc(layers[layer_index], layer_sizes[layer_index] * sizes_array_t[layer_index]);
+		auto realloc_result = realloc(layers[layer_index], layer_type_sizes[layer_index] * (layer_sizes[layer_index]));
 
-		if (!layers[layer_index])
+		if (!realloc_result)
 			return;//complain
 
-		auto cpy_result = memcpy(layers[layer_index] + (layer_sizes[layer_index] - count) * sizes_array_t[layer_index], data_ptr, count * sizes_array_t[layer_index]);
+		layers[layer_index] = realloc_result;
+
+		auto cpy_result = memcpy((u8*)(layers[layer_index]) + (layer_sizes[layer_index] - count) * layer_type_sizes[layer_index], data_ptr, count * layer_type_sizes[layer_index]);
 		
 		if (!cpy_result)
 			return;//complain
 	}
 }
 
-int main()
+namespace tree_struct_layer
 {
-	enum class tree_struct_layer : u8
+	enum tree_struct_layer : u8
 	{
 		vec3, color, str,
 	};
-	struct tree_sizes
+}
+int main()
+{
+	u64 tree_sizes[] = { sizeof(vec3), sizeof(color), sizeof(ben::str120) };
+	btl::tree<3> tree;
+
+	vec3 vector_data[] = { {1.0f, 2.5f, 4.5f}, {9.5f, 2.7f, 9.2f} };
+	tree.add(tree_sizes, tree_struct_layer::vec3, vector_data, 2);
+
+	for (u32 i = 0; i < tree.layer_count(); i++)
 	{
-		const u64 vec3 = sizeof(vec3);
-		const u64 color = sizeof(color);
-		const u64 str = sizeof(ben::str120);
-	};
-	btl::tree<tree_sizes, 3> tree;
+		ben::print("layer #%u has a count of %u elements each sized %u bytes\n", i, tree.layer_sizes[i], tree_sizes[i]);
+
+		if (tree.layer_sizes[i] > 0)
+		{
+			const u8* const layer_byte_data_ptr = reinterpret_cast<u8*>(tree.layers[i]);
+			for (const u8* layer_byte_ptr = layer_byte_data_ptr;
+				layer_byte_ptr < layer_byte_data_ptr + tree.layer_sizes[i] * tree_sizes[i];
+				layer_byte_ptr++)
+			{
+				ben::print("byte#%hhu : %hhx\n", layer_byte_ptr - layer_byte_data_ptr, *layer_byte_ptr);
+			}
+		}
+	}
 	
 	return 0;
 }
